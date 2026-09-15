59f292dc14b1fbefdb3f2e233f2d4b8b30e97548
surimchai <slchai0903@gmail.com>
2026-09-15 16:54:37 +0900

    MyCompFunc.jsx update

--- todolist_react/src/components/MyComponent.jsx
+++ todolist_react/src/components/MyComponent.jsx
@@ -7,10 +7,21 @@ class MyComponent extends Component {
         message: '',
         username: ''
     };
+    
+    //event handler 함수
+    handleChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        });
+    };
 
+
+    //Component 메서드 재정의
     render() {
         const { name, age } = this.props; 
         const { value, message, username } = this.state;
+        const { handleChange } = this;
+
         return (
             <div>
                 <h2>클래스 형태의 컴포넌트</h2>
@@ -18,10 +29,16 @@ class MyComponent extends Component {
                 <p>상태변수 value = {value}</p>
                 <button onClick={() => this.setState({
                     value: value + 1
-                })}>증가</button>
+                })}>증가</button> 
+                
+                <p>상태변수 message = {message}</p>
+                <input name="message" value={message} onChange={handleChange} />
+                <br />
+                <p>상태변수 username = {username}</p>
+                <input name="username" value={username} onChange={handleChange} />
             </div>
         );
-    }
+    }//render
 }
 
 export default MyComponent;
