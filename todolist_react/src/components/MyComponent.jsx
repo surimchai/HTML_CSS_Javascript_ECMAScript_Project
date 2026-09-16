e7d82193ebccb5997f36ea37b7463552a6d6a0b3
surimchai <slchai0903@gmail.com>
2026-09-16 10:34:23 +0900

    Form.css add

--- todolist_react/src/components/MyComponent.jsx
+++ todolist_react/src/components/MyComponent.jsx
@@ -1,26 +1,41 @@
 import React, { Component } from 'react';
+import './MyComponent.css';
 
 class MyComponent extends Component {
     //상태객체
     state = {
         value: 0,
         message: '',
-        username: ''
+        username: '',
+        isValid: false,
     };
     
     //event handler 함수
     handleChange = (e) => {
         this.setState({
+            //message: e.target.value
+            //username: e.target.value
             [e.target.name]: e.target.value
         });
+    };
+
+    handleEnter = (e) => {
+        if(e.keyCode === 13){
+            this.setState({
+                isValid: true,
+                message: ''
+            });
+            //html dom 직접 접근
+            this.myUsername.focus();
+        }//if
     };
 
 
     //Component 메서드 재정의
     render() {
         const { name, age } = this.props; 
-        const { value, message, username } = this.state;
-        const { handleChange } = this;
+        const { value, message, username, isValid } = this.state;
+        const { handleChange, handleEnter } = this;
 
         return (
             <div>
@@ -32,10 +47,15 @@ class MyComponent extends Component {
                 })}>증가</button> 
                 
                 <p>상태변수 message = {message}</p>
-                <input name="message" value={message} onChange={handleChange} />
+                <input name="message" value={message} onChange={handleChange}
+                    onKeyDown={handleEnter}
+                 />
                 <br />
                 <p>상태변수 username = {username}</p>
-                <input name="username" value={username} onChange={handleChange} />
+                <input name="username" value={username} onChange={handleChange} 
+                    className={isValid ? 'success':'failure'}
+                    ref={(ref) => this.myUsername = ref}
+                />
             </div>
         );
     }//render
